8514e01ca434adeefdacb03f99731b9034fa7104
isaac-fei <isaac.omega.fei@gmail.com>
2024-04-16 02:33:29 +0800

    Edit dt in plot_curve

--- explot/curve/curve.py
+++ explot/curve/curve.py
@@ -8,6 +8,7 @@ from ..types import RealVector, ComplexVector
 from .curve_arrow import CurveArrow
 
 
+ARROW_SIZE_FACTOR = 0.015
 ARROW_HEAD_LENGTH_TO_HEAD_WIDTH_RATIO = 1 / 0.618
 
 
@@ -19,7 +20,7 @@ def plot_curve(
     num_samples: int = 100,
     arrow_points: Optional[list[float]] = None,
     add_middle_arrow_point: bool = False,
-    dt: float = np.finfo(np.float64).eps,
+    dt: Optional[float] = None,
     ax: Optional[Axes] = None,
     **kwargs,
 ) -> None:
@@ -55,8 +56,28 @@ def plot_curve(
     for t in arrow_points:
         # Ensure that t must be in the range [t_min, t_max]
         assert (
-            t_min <= t <= t_max
-        ), f"Arrow point {t} is not in the range [{t_min}, {t_max}]"
+            t_min <= t < t_max
+        ), f"Arrow point {t} is not in the range [{t_min}, {t_max})"
+
+        if dt is None:
+            # Find the first index of the number in vector that is greater than t
+            # The value pointed by the index will be denoted as t1
+            # Its preceding value in the vector will be denoted as t0
+            # And dt will be set to t1 - t0
+            t1_index = find_first_value_greater_than(t, t_vector)
+
+            # Although t1_index is garanteed to be non-None since t is in the range [t_min, t_max)
+            assert (
+                t1_index is not None
+            ), f"Cannot find a value in the vector that is  greater than {t}"
+
+            # Index of t0
+            t0_index = t1_index - 1
+
+            # Calculate dt
+            t0 = t_vector[t0_index]
+            t1 = t_vector[t1_index]
+            dt = t1 - t0
 
         # Calculate the position of the arrow
         z = curve(t)
@@ -90,25 +111,16 @@ def add_arrow_to_line(
     # Color of the line
     color = line.get_color()
 
-    total_delta_x = ax.get_xlim()[1] - ax.get_xlim()[0]
-    total_delta_y = ax.get_ylim()[1] - ax.get_ylim()[0]
-    arrow_size = np.sqrt(total_delta_x**2 + total_delta_y**2) * 0.015
-
-    # Calculate the arrow head width
-    # arrow_head_with = line_width * ARROW_HEAD_WIDTH_TO_LINE_WIDTH_RATIO
-    arrow_head_width = arrow_size
-
-    # Calculate the arrow head length
-    arrow_head_length = ARROW_HEAD_LENGTH_TO_HEAD_WIDTH_RATIO * arrow_head_width
-
     # Create the arrow
     arrow = CurveArrow(
         x,
         y,
         dx,
         dy,
-        head_width=arrow_head_width,
-        head_length=arrow_head_length,
+        # Set dummy values for head width and head length
+        # since they will be updated later in the function `update_curve_arrows`
+        head_width=0,
+        head_length=0,
         # The color of the arrow is the same as the line
         color=color,
         # The zorder of the arrow is the same as the line
@@ -138,7 +150,7 @@ def update_curve_arrows(
     total_delta_y = ax.get_ylim()[1] - ax.get_ylim()[0]
 
     # Calculate the arrow size
-    arrow_size = np.sqrt(total_delta_x**2 + total_delta_y**2) * 0.015
+    arrow_size = np.sqrt(total_delta_x**2 + total_delta_y**2) * ARROW_SIZE_FACTOR
 
     # Calculate the arrow head width
     # arrow_head_with = line_width * ARROW_HEAD_WIDTH_TO_LINE_WIDTH_RATIO
@@ -168,3 +180,41 @@ def update_curve_arrows(
 
         # Add the new arrow to the axes
         ax.add_patch(arrow)
+
+
+def find_first_value_greater_than(
+    value: float,
+    sorted_array: RealVector,
+) -> Optional[int]:
+
+    start = 0
+    end = len(sorted_array) - 1
+
+    # Handle corner cases
+
+    if sorted_array[start] > value:
+        return start
+
+    if sorted_array[end] <= value:
+        return None
+
+    # Note that, now start < end
+
+    # Get the middle index
+    mid = (start + end) // 2
+
+    # The loop preserves the invariant that
+    # sorted_array[start] <= value < sorted_array[end]
+    while mid > start:
+
+        # Update the start or end index
+        if sorted_array[mid] > value:
+            end = mid
+        else:
+            start = mid
+
+        # Update the middle index
+        mid = (start + end) // 2
+
+    # Here, end is start + 1 since the middle index remains unchanged
+    return end
